63bfd0b987196b781df5b4e71e6b315bbc0cad3c
alex <souloflione@gmail.com>
2023-02-03 23:33:47 +0300

    Lesson8Runner

---
+++ src/LeassonEightRunner.java
@@ -0,0 +1,23 @@
+public class LeassonEightRunner {
+    public static void main(String[] args) {
+        double doubleValue = 12.98; // define + initialization
+        System.out.println(doubleValue);
+
+        int intValue; //define
+        intValue = 28; //initialization
+        System.out.println(intValue); //int
+
+        final byte byteValue = 39;
+        System.out.println(byteValue);
+
+        char charValue = 'q';
+        System.out.println(charValue);
+
+        boolean booleanValue = true;
+        System.out.println(booleanValue);
+
+        String stringValue = "Hello World!";
+        System.out.println(stringValue);
+
+    }
+}
