729e902fd72d1a0cce4c2e826186a455eed84b03
alex <souloflione@gmail.com>
2023-02-03 23:12:50 +0300

    Lesson7runner

---
+++ src/lessonSevenRunner.java
@@ -0,0 +1,11 @@
+public class lessonSevenRunner {
+    public static void main(String[] args) {
+        System.out.println(25); // int
+        System.out.println(25L); // long
+        System.out.println(23.879); // double
+        System.out.println(23.879F); // float
+        System.out.println('?'); // Char
+        System.out.println("?.1.111"); //String
+        System.out.println(false); //boolean
+    }
+}
