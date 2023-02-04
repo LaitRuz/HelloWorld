ff3411d5b0dde20cac827f6aa1bb788d6c1712f2
alex <souloflione@gmail.com>
2023-02-04 22:31:32 +0300

    Lesson14UpdateVersion1

---
+++ src/com/dev/leasson/lesson14/Lesson14Runner.java
@@ -0,0 +1,32 @@
+package com.dev.leasson.lesson14;
+
+public class Lesson14Runner {
+    public static void main(String[] args) {
+        // byte, short, int, char, String, enum;
+        int mouth = 6;
+        switch (mouth) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Winter");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Spring");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Summer");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Autumn");
+                break;
+            default:
+                System.out.println("Month is invalid");
+        }
+    }
+}
