1de12ca3c7dacd627355b4422521f3748277f411
alex <souloflione@gmail.com>
2023-02-04 22:45:50 +0300

    Lesson15UpdateVersion2

--- src/com/dev/leasson/lesson15/Lesson15.java
+++ src/com/dev/leasson/lesson15/Lesson15.java
@@ -2,8 +2,8 @@ package com.dev.leasson.lesson15;
 
 public class Lesson15 {
     public static void main(String[] args) {
-        int mouth = 9;
-       String result = switch (mouth) {
+        var mouth = 9;
+       var result = switch (mouth) {
             case 12, 1, 2 -> "Winter";
             case 3, 4, 5 -> "Spring";
             case 6, 7, 8 -> "Summer";
