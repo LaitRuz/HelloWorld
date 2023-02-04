6c53df5d3a61bb73293614391f2c9c11fa8dc028
alex <souloflione@gmail.com>
2023-02-04 22:42:45 +0300

    Lesson15UpdateVersion1

---
+++ src/com/dev/leasson/lesson15/Lesson15.java
@@ -0,0 +1,16 @@
+package com.dev.leasson.lesson15;
+
+public class Lesson15 {
+    public static void main(String[] args) {
+        int mouth = 9;
+       String result = switch (mouth) {
+            case 12, 1, 2 -> "Winter";
+            case 3, 4, 5 -> "Spring";
+            case 6, 7, 8 -> "Summer";
+            case 9, 10, 11 -> "Autumn";
+            default -> "Month is invalid";
+        };
+
+        System.out.println(result);
+    }
+}
