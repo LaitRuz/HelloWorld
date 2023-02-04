0b697f9f7c2ca604480510899700b134e65a2aea
alex <souloflione@gmail.com>
2023-02-04 22:12:26 +0300

    Lesson13Update

--- src/com/dev/leasson/lesson13/Lesson13Runner.java
+++ src/com/dev/leasson/lesson13/Lesson13Runner.java
@@ -1,4 +1,15 @@
 package com.dev.leasson.lesson13;
 
 public class Lesson13Runner {
+    public static void main(String[] args) {
+        int value = 90;
+        // boolean test = value > 0;
+        if (value > 0) {
+            System.out.println(value + " Positive value");
+        } else if (value < 0) {
+            System.out.println(value + " is not negative");
+        }  else {
+            System.out.println("zero");
+        }
+    }
 }
---
+++ src/com/dev/leasson/lesson13/TernaryOperator.java
@@ -0,0 +1,4 @@
+package com.dev.leasson.lesson13;
+
+public class TernaryOperator {
+}
