690056ec71d570f3aee644df987eca7a09001e39
alex <souloflione@gmail.com>
2023-02-05 12:36:32 +0300

    Lesson16UpdateVersion3

--- src/com/dev/leasson/lesson16/Task1.java
+++ src/com/dev/leasson/lesson16/Task1.java
@@ -4,7 +4,7 @@ package com.dev.leasson.lesson16;
 * Порядковый номер месяца. Вывести на консоль пору года, к которой относится месяц.
 */
 public class Task1 {
-    public static void main(String[] args) {
+  /*  public static void main(String[] args) {
         int mouth = 10;
         if (mouth == 12 || mouth == 1 || mouth == 2) {
             System.out.println("Winter");
@@ -13,6 +13,21 @@ public class Task1 {
         } else if (mouth == 6 || mouth == 7 || mouth == 8) {
             System.out.println("Summer");
         } else if (mouth == 9 || mouth == 10 || mouth == 11){
+            System.out.println("Autumn");
+        } else {
+            System.out.println("Mouth is invalid " + mouth);
+        }
+    } */
+
+    public static void main(String[] args) {
+        int mouth = 10;
+        if (mouth == 12 || mouth == 1 || mouth == 2) {
+            System.out.println("Winter");
+        } else if (3 <= mouth && mouth <= 5) {
+            System.out.println("Spring");
+        } else if (6 <= mouth && mouth <= 8) {
+            System.out.println("Summer");
+        } else if (9 <= mouth && mouth <= 11){
             System.out.println("Autumn");
         } else {
             System.out.println("Mouth is invalid " + mouth);
