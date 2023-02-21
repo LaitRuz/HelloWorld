453c3ac1b5448b94842fe7b5f8eb9ebddde310c5
Alex <souloflione@gmail.com>
2023-02-21 23:39:30 +0300

    lesson24version1

---
+++ src/com/dev/leasson/lesson24/lesson24Runner.java
@@ -0,0 +1,39 @@
+package com.dev.leasson.lesson24;
+
+public class lesson24Runner {
+    public static void main(String[] args) {
+        int [][] values = new int[3][];
+        values[0] = new int[7];
+        values[0][3] = 97;
+        System.out.println(values.length); //3
+        System.out.println(values[1]);
+        System.out.println(values[0]);
+        System.out.println(values[0].length);
+        System.out.println(values.length);
+        System.out.println(values[0][1]); //0
+        System.out.println(values[0][3]); //97
+
+
+    }
+//    public static void main(String[] args) {
+//        int[][] values = new int[3][4];
+//        System.out.println(values.length); //3
+//        System.out.println(values[1].length); //4
+//        System.out.println(values[1][3]); //0
+//    }
+
+
+    private static void example1(String[] args) {
+        int[][] values = {
+                { -1, 0}, //0
+                null,
+                {8, 65,34, -6}, //2
+                {} // 3
+        };
+        System.out.println(values.length); //4
+        System.out.println(values[0].length); //2
+        System.out.println(values[0][1]); //0
+        System.out.println(values[2][2]); //34
+        System.out.println(values[1]); // null
+    }
+}
